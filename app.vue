5b07ae4c250b0b147b648f958cdb29d2114776fb
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-09-06 16:02:07 -0300

    feat: add tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div
+    class="bg-slate-200 dark:bg-slate-900 w-screen min-h-screen flex justify-center items-center"
+  >
+    <main
+      class="bg-white dark:bg-slate-800 max-w-md flex justify-center items-center shadow-xl rounded-xl p-8"
+    >
+      <h1>Hello World</h1>
+    </main>
   </div>
 </template>
